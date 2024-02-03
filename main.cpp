4c509f61efeace5d8b8da940b13e885a73ceee97
urzk <shgerk@gmail.com>
2024-02-04 05:17:21 +0900

    input (n, L), complex matrix => real matrix

--- main.cpp
+++ main.cpp
@@ -1,40 +1,37 @@
 #include <cassert>
-#include <complex>
 #include <ctime>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <vector>
 #include "Eigen/Eigen"
 
 using namespace std;
 using namespace Eigen;
 
-typedef complex<double> cd;
-typedef Triplet<cd> Tcd;
+typedef Triplet<double> Td;
 
-const int L = 10;
-const int n = 2;
 const double J = 1.0;
 
-const int N = pow(n, L);
-const double s = (n - 1.0) / 2.0;
+int n;
+int L;
 
-MatrixXcd generate_single(vector<Tcd> S)
+MatrixXcd generate_single(vector<Td> S)
 {
-    SparseMatrix<cd> M(n, n);
+    SparseMatrix<double> M(n, n);
     M.setFromTriplets(S.begin(), S.end());
     return M;
 }
 
-SparseMatrix<cd> generate(int i, vector<Tcd> S)
+SparseMatrix<double> generate(int i, vector<Td> S, int N)
 {
     assert(i < L);
-    SparseMatrix<cd> M(N, N);
+    // n進数で右からi桁目の値が、i番目の格子に対応
+    SparseMatrix<double> M(N, N);
     int i_shift = pow(n, i);
     int upper_shift = i_shift * n;
     int lower_size = i_shift;
     int upper_size = N / upper_shift;
-    for (Tcd x : S)
+    for (Td x : S)
     {
         assert(x.row() < n);
         assert(x.col() < n);
@@ -58,23 +55,28 @@ int main()
 {
     clock_t start = clock();
 
-    vector<Tcd> I, Sx, Sy, Sz;
+    cout << "input (n L): ";
+    cin >> n >> L;
+
+    assert(pow(n, L) < INT_MAX);
+    int N = pow(n, L);
+    double s = (n - 1.0) / 2.0;
+
+    vector<Td> I, Sz, Sp, Sm;
     for (int i = 0; i < n; i++)
     {
-        I.push_back(Tcd(i, i, 1.0));
+        I.push_back(Td(i, i, 1.0));
         double sz = s - i;
-        Sz.push_back(Tcd(i, i, sz));
+        Sz.push_back(Td(i, i, sz));
         if (i >= 1)
         {
-            double a = sqrt(i * (n - i)) / 2.0;
-            Sx.push_back(Tcd(i - 1, i, a));
-            Sx.push_back(Tcd(i, i - 1, a));
-            Sy.push_back(Tcd(i - 1, i, a * cd(0.0, -1.0)));
-            Sy.push_back(Tcd(i, i - 1, a * cd(0.0, 1.0)));
+            double a = sqrt(i * (n - i) / 2.0);
+            Sp.push_back(Td(i - 1, i, a));
+            Sm.push_back(Td(i, i - 1, a));
         }
     }
 
-    SparseMatrix<cd> H(N, N);
+    SparseMatrix<double> H(N, N);
     for (int i = 0; i < L; i++)
     {
         int j = i + 1;
@@ -82,37 +84,37 @@ int main()
         {
             j %= L;
         }
-        SparseMatrix<cd> SiSj = generate(i, Sx) * generate(j, Sx) + generate(i, Sy) * generate(j, Sy) + generate(i, Sz) * generate(j, Sz);
+        SparseMatrix<double> SiSj = generate(i, Sp, N) * generate(j, Sm, N) + generate(i, Sm, N) * generate(j, Sp, N) + generate(i, Sz, N) * generate(j, Sz, N);
         H += SiSj;
     }
     H *= J;
 
-    SparseMatrix<cd> Sx_total(N, N), Sy_total(N, N), Sz_total(N, N);
+    SparseMatrix<double> Sp_total(N, N), Sm_total(N, N), Sz_total(N, N);
     for (int i = 0; i < L; i++)
     {
-        Sx_total += generate(i, Sx);
-        Sy_total += generate(i, Sy);
-        Sz_total += generate(i, Sz);
+        Sp_total += generate(i, Sp, N);
+        Sm_total += generate(i, Sm, N);
+        Sz_total += generate(i, Sz, N);
     }
-    SparseMatrix<cd> S_total = Sx_total * Sx_total + Sy_total * Sy_total + Sz_total * Sz_total;
+    SparseMatrix<double> S_total = Sp_total * Sm_total + Sm_total * Sp_total + Sz_total * Sz_total;
 
     cout << fixed;
 
-    // SelfAdjointEigenSolver<SparseMatrix<cd>> es_s;
+    // SelfAdjointEigenSolver<SparseMatrix<double>> es_s;
     // es_s.compute(10 * S_total + Sz_total);
     // cout << "eigenvalues(S, Sz):" << endl;
     // cout << es_s.eigenvalues()(seqN(0, 20)) << endl;
     // cout << "eigenvectors:" << endl;
     // cout << es_s.eigenvectors() << endl;
 
-    SelfAdjointEigenSolver<SparseMatrix<cd>> es;
+    SelfAdjointEigenSolver<SparseMatrix<double>> es;
     es.compute(H);
     clock_t end = clock();
     std::cout << "calculation time: " << (double)(end - start) / CLOCKS_PER_SEC << "s." << endl;
 
     cout << "eigenvalues:" << endl;
-    cout << es.eigenvalues()(seqN(0, 8)) << endl;
-    cout << "spin gap: " << es.eigenvalues()[1] - es.eigenvalues()[0] << endl;
+    cout << es.eigenvalues()(seqN(0, min(N, 8))) << endl;
+    cout << "spin gap: Δ = " << es.eigenvalues()[1] - es.eigenvalues()[0] << "." << endl;
     // cout << "eigenvectors:" << endl;
     // cout << (es_s.eigenvectors().adjoint() * es.eigenvectors())(all, seqN(0, 4)) << endl;
 
